3073162f3ae82328076f343c40fc2860e590cc56
Ana Beatriz <ana.beatriz@eurekalabs.com.br>
2021-06-22 09:59:35 -0300

    Segunda parte da segunda aula feito com sucesso

--- aulas/letmeask/src/pages/Home.tsx
+++ aulas/letmeask/src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import { useHistory } from 'react-router-dom'
+import { useAuth } from '../hooks/useAuth';
 
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
@@ -6,21 +7,18 @@ import googleIconImg from '../assets/images/google-icon.svg';
 
 import { Button } from '../components/Button';
 
+
 import '../styles/auth.scss'
-import {firebase, auth, database } from '../services/firebase';
 
 export function Home() {
     const history = useHistory();
+    const { user, signInWithGoogle } = useAuth();
 
-
-    function handleCreateRoom() {
-        const provider = new firebase.auth.GoogleAuthProvider();
-
-        auth.signInWithPopup(provider).then((result) => {
-            console.log(result);
-            history.push('/rooms/new');
-        })
-
+    async function handleCreateRoom() {
+        if (!user) {
+            await signInWithGoogle();
+        }
+        history.push('/rooms/new');
     }
 
     return (
@@ -31,7 +29,7 @@ export function Home() {
                 <p>Tire as dúvidas da sua audiência em tempo-real</p>
             </aside>
             <main>
-                <div  className="main-content">
+                <div className="main-content">
                     <img src={logoImg} alt="Letmeask" />
                     <button onClick={handleCreateRoom} className="create-room">
                         <img src={googleIconImg} alt="Logo do Google" />
@@ -39,8 +37,8 @@ export function Home() {
                     </button>
                     <div className="separator">ou entre em uma sala</div>
                     <form>
-                        <input 
-                            type="text" 
+                        <input
+                            type="text"
                             placeholder="Digite o código da sala"
                         />
                         <Button type="submit">
