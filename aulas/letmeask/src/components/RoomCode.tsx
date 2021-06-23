7afee5c6e2c3d7a1499da8d68cbdd8ed223f44fc
Ana Beatriz <ana.beatriz@eurekalabs.com.br>
2021-06-23 20:32:02 -0300

    Feito segunda parte da terceira aula com sucesso

--- aulas/letmeask/src/components/RoomCode.tsx
+++ aulas/letmeask/src/components/RoomCode.tsx
@@ -2,13 +2,22 @@ import copyImg from '../assets/images/copy.svg';
 
 import '../styles/roomCode.scss'
 
-export function RoomCode() {
+type RoomCodeProps = {
+    code: string;
+}
+
+export function RoomCode(props: RoomCodeProps) {
+
+    function copyRoomCodeToClipboard() {
+        navigator.clipboard.writeText(props.code)
+    }
+
     return (
-        <button className="room-code">
+        <button className="room-code" onClick={copyRoomCodeToClipboard}>
             <div>
                 <img src={copyImg} alt="Copy room code" />
             </div>
-            <span>Sala #123455667788</span>
+            <span>Sala #{props.code}</span>
         </button>
     )
 }
